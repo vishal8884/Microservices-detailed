c9fae1b2ec3031604d50232a19892e3bac122ede
vishal <vishalambekar459@gmail.com>
2023-02-13 23:54:09 +0530

    level 2 notes added

--- level 1/movie-catalog-service/src/main/java/io/vishal/MovieCatalogServiceApplication.java
+++ level 1/movie-catalog-service/src/main/java/io/vishal/MovieCatalogServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -19,7 +20,10 @@ public class MovieCatalogServiceApplication {
 	@Bean
 	@LoadBalanced //URL which is given is a hint which service needs to be discovered
 	public RestTemplate getRestTempleate() {
-		return new RestTemplate();
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectionRequestTimeout(3000);
+		
+		return new RestTemplate(clientHttpRequestFactory);
 	}
 	
 	
